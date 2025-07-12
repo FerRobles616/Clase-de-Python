fb5fa86bb46de1c4b7c037a9e05de94066342ce5
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-12 13:00:03 -0300

    Finalizado Entradas

---
+++ Clase 2/Entrada.py
@@ -0,0 +1,11 @@
+"""
+Entrada de datos por teclado
+"""
+
+
+cadena = input("Ingrese nombre de su proyecto: ")
+print("El nombre de su proyecto es:", {cadena})
+
+numero = int(input("Ingrese un numero: "))
+print("El numero ingresado es:", {numero})
+
