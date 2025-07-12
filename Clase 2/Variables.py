d39298d293ab1c99c649077f060c37e775fa0418
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-12 11:26:42 -0300

    Finalizado Variables

---
+++ Clase 2/Variables.py
@@ -0,0 +1,24 @@
+print("hola mundo")
+entero= 10
+flotante= 10.5
+cadena= "hola"
+booleano= True
+print(entero, flotante, cadena, booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+
+print("sumando de dos numeros")
+# Operaciones básicas con variables
+# Suma, resta, multiplicación y división
+a=10
+b=20
+print("suma de dos numeros", a+b)
+print("restando de dos numeros")
+print("resta de dos numeros", a-b)
+print("multiplicando de dos numeros")
+print("multiplicacion de dos numeros", a*b)
+b=2
+print("dividiendo de dos numeros")
+print("division de dos numeros", a/b)
+
+
+
