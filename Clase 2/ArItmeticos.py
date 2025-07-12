3b5cabcd1be0f382a56107a820f7017071de3ede
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-12 11:54:26 -0300

    Finalizado Aritmeticos

---
+++ Clase 2/ArItmeticos.py
@@ -0,0 +1,26 @@
+"""
+Operaciones aritmeticas basicas en Python
+sumamos, restamos, multiplicamos y dividimos dos numeros
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+"""
+a = 17 #se pide al usuario que ingrese un numero
+b = 18
+r = 0
+r = a + b
+print("Suma:", r)
+r = a - b
+print("Resta:", r)
+r = a * b
+print("Multiplicación:", r)
+r = a / b
+print("División:", r)
+r = a % b
+print("Módulo:", r)
+r = a ** b
+print("Exponente:", r)
+# Operadores de asignación
