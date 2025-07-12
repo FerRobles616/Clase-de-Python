abff0c0b3139160021eba31bcd2c58958db6f75b
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-12 13:48:46 -0300

    Entrada Finalizado

--- Clase 2/Entrada.py
+++ Clase 2/Entrada.py
@@ -4,8 +4,7 @@ Entrada de datos por teclado
 
 
 cadena = input("Ingrese nombre de su proyecto: ")
-print("El nombre de su proyecto es:", {cadena})
+print(f"El nombre de su proyecto es:, {cadena}")
 
-numero = int(input("Ingrese un numero: "))
-print("El numero ingresado es:", {numero})
-
+numero = int(input("¿Que version es? "))
+print(f"La version de su proyecto es:, {numero+1}")
