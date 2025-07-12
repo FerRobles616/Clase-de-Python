fb6299c80db868b30551d2eb72da5f629e78ba0d
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-12 11:42:31 -0300

    Finalizado Operadores

---
+++ Clase 2/Operadores.py
@@ -0,0 +1,23 @@
+"""
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+#operadores de comparacion
+"""
+
+a=17 #se pide al usuario que ingrese un numero
+b=18
+r=a == b
+r= a != b
+print(r)
+r=a > b
+print("Es menor", r)
+r=a < b
+print("Es mayor", r)
+r=a <= b
+print(r)
+r=a >= b
+print(r)
