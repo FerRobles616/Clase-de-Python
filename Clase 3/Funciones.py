079d1ec6f1ccb8fef932b8ea9372a02e3e58ca48
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-16 23:07:36 -0300

    a

---
+++ Clase 3/Funciones.py
@@ -0,0 +1,17 @@
+"""
+Ejemplo de funciones en Python.
+"""
+def sumar(5, 3):
+    return a + b  # Retorna la suma de dos números
+
+
+resultado = sumar(a,b)  # Llama a la función con los argumentos 5 y 3
+print("La suma es:", resultado)  # Imprime el resultado de la suma
+
+
+def restar(a, b):
+    return a - b  # Retorna la resta de dos números
+
+
+resultado = restar(10, 4)  # Llama a la función con los argumentos 10 y 4
+print("La resta es:", resultado)  # Imprime el resultado de la resta
