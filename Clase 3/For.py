ef7aa19ee1cad7ac23da999dc401445ef70d2565
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-16 22:55:57 -0300

    Finalizado For

--- Clase 3/For.py
+++ Clase 3/For.py
@@ -1,8 +1,12 @@
 """
-Ejemplo estructura de control For en Python.
+# Ejemplo estructura de control For
 """
-array = ["futbol", "Pc", 18.6, 18, [6, 7, 10, 4], True, False]
-print(len(array))  # Imprime el contenido de la lista
-for i in range(len(array)):  # Itera desde 0 hasta 5
-    print("Valor de i:", i)  # Imprime el valor actual de i
+for i in range(6):  # Itera desde 0 hasta 5
+    print("Contador:", i)  # Imprime el valor actual del contador
 print("Fin del bucle for")  # Mensaje al finalizar el bucle
+
+array = ["futbol", "Pc", 18.6, 18, [6, 7, 10, 4], True, False]
+array.append("Baloncesto")  # Agrega un nuevo elemento a la lista
+for i in range(len(array)):  # Itera sobre cada elemento de la lista
+    print("Elemento en la posición", i, ":", array[i])  # Imprime el elemento actual
+print("Fin del bucle for sobre lista")  # Mensaje al finalizar el bucle
