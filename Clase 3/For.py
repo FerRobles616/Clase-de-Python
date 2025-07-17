f1d6fe8a28544c11acce6a8493faab8377e44a8b
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-16 22:37:06 -0300

    .

---
+++ Clase 3/For.py
@@ -0,0 +1,7 @@
+"""
+Ejemplo estructura de control For en Python.
+"""
+
+for i in range(6):  # Itera desde 0 hasta 5
+    print("Valor de i:", i)  # Imprime el valor actual de i
+print("Fin del bucle for")  # Mensaje al finalizar el bucle
--- Clase 3/While.py
+++ Clase 3/While.py
@@ -5,4 +5,4 @@ contador = 0  # Inicializa el contador en 0
 while contador <= 5:  # Mientras el contador sea menor que 5
     print("Contador:", contador)  # Imprime el valor actual del contador
     contador += 1  # Incrementa el contador en 1
-    print("Fin del bucle while")  # Mensaje al finalizar el bucle
+print("Fin del bucle while")  # Mensaje al finalizar el bucle
