5f298465af0f493fd40e0a663bc1fbde12bf34bb
Fernando José Robles Ledezma <fernandojrl808@@Gmail.com>
2025-07-16 22:32:07 -0300

    Finalizado  While

---
+++ Clase 3/While.py
@@ -0,0 +1,8 @@
+"""
+Estructura de control While
+"""
+contador = 0  # Inicializa el contador en 0
+while contador <= 5:  # Mientras el contador sea menor que 5
+    print("Contador:", contador)  # Imprime el valor actual del contador
+    contador += 1  # Incrementa el contador en 1
+    print("Fin del bucle while")  # Mensaje al finalizar el bucle
