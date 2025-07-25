6808a3a682c4d09b45d5ab4b8e13729416259922
FerRobles616 <fernandojrl808@gmail.com>
2025-07-25 00:08:07 -0300

    cliente.py

--- cliente.py
+++ cliente.py
@@ -6,40 +6,50 @@ cliente= Blueprint('cliente', __name__)
 @cliente.route('/cliente', methods=['POST'])
 
 def llamarServicioSet():
-    ci= request.json.get('ci')
+ ci= request.json.get('ci')
     print("ci enviado:", ci)
 
-    codRes, menRes, accion = inicializarVariables(ci)
+    codRes, menRes, accion, nombre, apellido = inicializarVariables(ci)
+
+    if codRes == 'ERROR':
+        ci = "4133267"
 
     salida = {
         'codRes': codRes,
         'menRes': menRes,
-        'cedula': ci,
+        'ci': ci,
         'accion': accion,
+        'nombre': nombre,
+        'apellido': apellido
     }
 
     return jsonify(salida)
 
 
-def inicializarVariables(ci):
+def inicializarVariables(cedula):
     accion = "Success"
     codRes = 'SIN_ERROR'
     menRes = "OK"
-    cedula= "4133266"
+    ci= "4133266"
     nombre = "Derlis"
     apellido= "Caballero Mendoza"
 
     try:
-        if ci == cedula:
+        if cedula == ci:
             print("Success")
             accion = "Success"
+        else:
+            codRes = 'ERROR'
+            menRes = "Cliente no encontrado"
+            accion = "Cliente no está en el sistema"
+            ci = "4133267"
+            nombre = ""
+            apellido = ""
     except Exception as e:
         accion = "Cliente no está en el sistema"
         codRes = "ERROR"
         menRes = "Error cliente"
         ci = "4133267"
         return codRes, menRes, accion,
-    
-    
-    
-    return codRes, menRes, accion,
+
+    return codRes, menRes, accion, nombre, apellido
